aa7c09e9595880d45035f2a2e57af81ff9bc0309
RomeoAndJulietAndMe <2710457537@qq.com>
2019-07-05 00:45:14 +0800

    '可能要退回'

--- js/index.js
+++ js/index.js
@@ -41,6 +41,13 @@ $(window).scroll(function(){
 
 });
 
+//专区
+$('.shou_zhuanqu').onloadAnimate(function(){
+    this.find('li').eq(0).addClass('animated fadeInUpSmall').next().addClass('animated fadeInUpSmall').css('animation-delay','.2s').next().addClass('animated fadeInUpSmall').css('animation-delay','.4s').next().addClass('animated fadeInUpSmall').css('animation-delay','.6s')   
+},30);
+/* $('.shou_zhuanqu li').onloadAnimate(function(){
+    this.addClass('animated fadeInUp');
+}); */
 
 // $('.shou_kingdom_imgs').on("mouseover",".shou_kingdom_img",function () {
 //     var _this = $(this);
@@ -60,3 +67,19 @@ $('.shou_kingdom_imgs').mouseover(function (e){
 }) ;
 
  
+//高端零食
+/* $('.c_move_left').onloadAnimate(function(){
+    this.addClass('animated fadeInLeft').css('animation-delay','.3s')
+},30);
+$('.c_move_right').onloadAnimate(function(){
+    this.find('p').eq(0).addClass('animated fadeInRightSmall').css('animation-delay','.3s').next().addClass('animated fadeInRightSmall').css('animation-delay','.5s').next().addClass('animated fadeInRightSmall').css('animation-delay','.7s')
+},30);
+$('.c_move_up').onloadAnimate(function(){
+    this.addClass('animated fadeInUpSmall').find('h2').addClass('animated fadeInUpSmall').css('animation-delay','.3s').next().addClass('animated fadeInUpSmall').css('animation-delay','.5s')
+},30);
+$('.c_move_right-left').onloadAnimate(function(){
+    this.addClass('animated fadeInRight').css('animation-delay','.3s')
+},30);
+$('.c_move_right-right').onloadAnimate(function(){
+    this.find('p').eq(0).addClass('animated fadeInRight').css('animation-delay','.3s').next().addClass('animated fadeInRight').css('animation-delay','.5s').next().addClass('animated fadeInRight').css('animation-delay','.7s')
+},30); */
